ea02cf07194775f5fafc5e48efbf095d64b2138a
Sayed Sadeed <sayed.sadeed@gmail.com>
2020-01-11 13:40:20 +1000

    fixed routing issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { styled } from 'styled-components'
-import { Switch, Route, useParams, Redirect, BrowserRouter as Router } from 'react-router-dom'
+import { Switch, Route, useParams, BrowserRouter as Router } from 'react-router-dom'
 
 const Home = () => (
   <div>
@@ -32,7 +31,7 @@ function App({ className }) {
         <div className="column content">
           <div className="bottom">
             <Switch>
-              <Redirect exact path="/" component={Home} />
+              <Route exact path="/" component={Home} />
               <Route path="/friends/:name" component={Friends} />
               <Route component={NotFound} />
             </Switch>
@@ -43,27 +42,4 @@ function App({ className }) {
   )
 }
 
-export default styled(App)`
-  height: 100%;
-  overflow: hidden; /*makes the body non-scrollable (we will add scrolling to the sidebar and main content containers)*/
-  margin: 0px; /*removes default style*/
-  display: flex; /*enables flex content for its children*/
-  box-sizing: border-box;
-
-  .column {
-    height: 100%; /*allows both columns to span the full height of the browser window*/
-    display: flex;
-    flex-direction: column; /*places the left and right headers above the bottom content*/
-  }
-
-  .content {
-    width: 100%;
-    flex-basis: 100%;
-    margin-top: 60px;
-  }
-
-  .bottom {
-    flex-grow: 1; /*ensures that the container will take up the full height of the parent container*/
-    overflow-y: auto; /*adds scroll to this container*/
-  }
-`
+export default App
