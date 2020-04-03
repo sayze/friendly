f156f0518bf124f5832e750a83a9f185e28599a3
Sayed Sadeed <sayed.sadeed@gmail.com>
2020-04-03 19:09:07 +1000

    updated all deps

---
+++ src/helloworld.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HelloWorld = () => <h1>Hello World !</h1>
+
+export default {
+  title: 'Hello World',
+  component: HelloWorld,
+}
+
+export const Default = () => <HelloWorld />
