7d27149e55231a85309265702499c8ba4da15f31
Sayed Sadeed <sayed@foundu.com.au>
2020-06-30 23:49:13 +1000

    remove context from friend list

--- src/components/Friend/List.js
+++ src/components/Friend/List.js
@@ -2,22 +2,11 @@ import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { Container, Row, Col } from 'react-bootstrap'
 import Tile from './Tile'
-import Context from 'services/FilterContext'
-
-const filterFriends = (friends, { search }) => {
-  if (search.trim().length === 0) {
-    return friends
-  }
-  const query = search.toLowerCase()
-  return friends.filter(friend => friend.name.toLowerCase().indexOf(query) > -1)
-}
 
 const List = ({ friends }) => {
-  const { filters } = useContext(Context)
-
   return (
     <Container>
-      {filterFriends(friends, filters).map(friend => (
+      {friends.map(friend => (
         <Row>
           <Col>
             <Tile image={friend.img} name={friend.name} subtext={friend.info} />
