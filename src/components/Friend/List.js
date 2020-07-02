4512e97af26aa34d0f415c95198bcf47522ffb37
Sayed Sadeed <sayed.sadeed@gmail.com>
2020-07-02 15:53:53 +1000

    added no content message to friend list

--- src/components/Friend/List.js
+++ src/components/Friend/List.js
@@ -1,20 +1,32 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Container, Row, Col } from 'react-bootstrap'
-import Tile from './Tile'
+import Tile from '.'
+import NoContent from 'components/UI/NoContent'
+
+const renderFriends = friends =>
+  friends.length > 0 ? (
+    friends.map(friend => (
+      <Row className="my-2">
+        <Col>
+          <Tile image={friend.img} name={friend.name} subtext={friend.info} />
+        </Col>
+      </Row>
+    ))
+  ) : (
+    <Row className="my-5">
+      <Col>
+        <NoContent
+          icon="sad-cry"
+          title="You have no friends"
+          message="You can start adding friends by pressing the button up top ^^"
+        />
+      </Col>
+    </Row>
+  )
 
 const List = ({ friends }) => {
-  return (
-    <Container>
-      {friends.map(friend => (
-        <Row className="my-2">
-          <Col>
-            <Tile image={friend.img} name={friend.name} subtext={friend.info} />
-          </Col>
-        </Row>
-      ))}
-    </Container>
-  )
+  return <Container>{renderFriends(friends)}</Container>
 }
 
 List.defaultProps = {
