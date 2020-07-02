10e3360d9ce3d828b51530e757f4ea7ea777b265
Sayed Sadeed <sayed.sadeed@gmail.com>
2020-07-02 13:20:51 +1000

    swapped manual friend gen with helper

--- src/components/Friend/List.stories.js
+++ src/components/Friend/List.stories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import faker from 'faker'
 import List from './List'
 import { Provider } from 'services/FilterContext'
+import { fakeFriends } from './utils'
 
 export default {
   title: 'Friend/List',
@@ -9,17 +10,4 @@ export default {
   decorators: [storyFn => <Provider>{storyFn()}</Provider>],
 }
 
-const generateFriends = qty => {
-  const friends = []
-  for (let i = 0; i < qty; i++) {
-    friends.push({
-      id: faker.random.uuid(),
-      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-      info: 'Last updated 20 minutes ago',
-      img: faker.image.avatar(),
-    })
-  }
-  return friends
-}
-
-export const Default = () => <List friends={generateFriends(6)} />
+export const Default = () => <List friends={fakeFriends(4)} />
