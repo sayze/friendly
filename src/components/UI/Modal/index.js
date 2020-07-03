2d2aeca21d9cc32281ba8081e8a6e9b04dea6553
Sayed Sadeed <sayed.sadeed@gmail.com>
2020-07-03 11:41:44 +1000

    added close dispatch at modal level

--- src/components/UI/Modal/index.js
+++ src/components/UI/Modal/index.js
@@ -3,10 +3,15 @@ import { Modal as UIModal } from 'react-bootstrap'
 import { ModalContext } from 'services/providers'
 
 const Modal = () => {
-  const { state } = useContext(ModalContext)
+  const { state, dispatch } = useContext(ModalContext)
 
   return (
-    <UIModal show={state.open} onHide={state.onClose}>
+    <UIModal
+      show={state.open}
+      onHide={() => {
+        dispatch({ type: 'close' })
+      }}
+    >
       <UIModal.Header closeButton>
         <UIModal.Title>{state.title}</UIModal.Title>
       </UIModal.Header>
