d6f9179fe30a313ad90e4ec371181fb2c6a52286
Sayed Sadeed <sayed.sadeed@gmail.com>
2020-07-03 13:05:23 +1000

    file upload dialog working

--- src/components/UI/ImgUpload/index.js
+++ src/components/UI/ImgUpload/index.js
@@ -2,18 +2,32 @@ import React from 'react'
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import PropTypes from 'prop-types'
-import { Image } from 'react-bootstrap'
+import { FormControl, Image } from 'react-bootstrap'
 
-const ImgUpload = ({ image }) => (
-  <div className="img-upload">
-    {image.length > 0 ? (
-      <Image fluid src={image} roundedCircle />
-    ) : (
-      <FontAwesomeIcon className="img-upload__avatar" icon="user" size="5x" />
-    )}
-    <FontAwesomeIcon className="img-upload__camera text-secondary" icon="camera" size="2x" />
-  </div>
-)
+const ImgUpload = ({ image, onChange }) => {
+  const handleClick = event => {
+    document.getElementById('hiddenFileInput').click()
+  }
+
+  return (
+    <div onClick={handleClick} className="img-upload">
+      <FormControl
+        id="hiddenFileInput"
+        className="d-none"
+        type="file"
+        name="image-file"
+        onChange={onChange}
+        accept="image/png, image/jpeg"
+      />
+      {image.length > 0 ? (
+        <Image fluid src={image} roundedCircle />
+      ) : (
+        <FontAwesomeIcon className="img-upload__avatar" icon="user" size="5x" />
+      )}
+      <FontAwesomeIcon className="img-upload__camera text-secondary" icon="camera" size="2x" />
+    </div>
+  )
+}
 
 ImgUpload.defaultProps = {
   image: '',
