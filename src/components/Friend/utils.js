6b937c05bd0384e2d99ca90c1570602aaa63a21b
Sayed Sadeed <sayed.sadeed@gmail.com>
2020-07-03 17:23:40 +1000

    added param for generating blank profile images in faker

--- src/components/Friend/utils.js
+++ src/components/Friend/utils.js
@@ -5,15 +5,18 @@ import faker from 'faker'
  * @param qty
  * @returns {[]}
  */
-export const fakeFriends = qty => {
+export const fakeFriends = (qty = 0, noImage = 0) => {
   const friends = []
-  for (let i = 0; i < qty; i++) {
+  let noImageCt = 0
+
+  for (let i = 0; i <= qty; i++) {
     friends.push({
       id: faker.random.uuid(),
       name: `${faker.name.firstName()} ${faker.name.lastName()}`,
       info: 'Last updated 20 minutes ago',
-      img: faker.image.avatar(),
+      img: noImageCt <= noImage ? faker.image.avatar() : '',
     })
+    noImageCt++
   }
   return friends
 }
