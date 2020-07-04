876a24ad9606c7a4543364e41e3ec9b00dcd62a9
Sayed Sadeed <sayed.sadeed@gmail.com>
2020-07-04 13:49:03 +1000

    fixed delete button state

--- src/components/Friend/Delete.js
+++ src/components/Friend/Delete.js
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import PropTypes from 'prop-types'
 
-const Delete = ({ onSubmit, friend }) => (
-  <div>
-    <p>
-      Are you sure you want to delete <strong>{friend.name}</strong>? Action can't be undone.
-    </p>
-    <Button className="mt-2 float-right" variant="danger" onClick={() => onSubmit(friend)}>
-      Yes, Delete
-    </Button>
-  </div>
-)
+const Delete = ({ onSubmit, friend, submitting }) => {
+  const [isSubmitting, setIsSubmitting] = useState(submitting)
+
+  const handleSubmit = () => {
+    setIsSubmitting(true)
+    onSubmit(friend)
+  }
+
+  return (
+    <div>
+      <p>
+        Are you sure you want to delete <strong>{friend.name}</strong>? Action can't be undone.
+      </p>
+      <Button disabled={isSubmitting} className="mt-2 float-right" variant="danger" onClick={handleSubmit}>
+        Yes, Delete
+      </Button>
+    </div>
+  )
+}
 
 Delete.defaultProps = {
   onSubmit: () => {},
+  submitting: false,
 }
 
 Delete.propTypes = {
   friend: PropTypes.object.isRequired,
+  submitting: PropTypes.bool,
   onSubmit: PropTypes.func,
 }
 
